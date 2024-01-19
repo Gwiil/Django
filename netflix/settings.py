80cefcc46274254f389a3609212971f8b1b0e2ef
Gwiil <guilhermemotta122@gmail.com>
2024-01-18 21:16:37 -0300

    requirements

--- netflix/settings.py
+++ netflix/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_PRIVATE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
